b44a3c5decb23b0f689aefebd6e418c8c383415c
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-14 23:13:25 +0800

    PageController

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -8,6 +8,12 @@ class ViewDemo extends StatelessWidget {
       reverse: true,
       scrollDirection: Axis.vertical,
       onPageChanged: (currentPage) => debugPrint('Page:$currentPage'),
+      controller: PageController(
+        initialPage: 0, //默认显示第几个页面（默认0）
+        keepPage: false,
+        // 是否保存当前 Page 的状态，如果保存，下次回复对应保存的 page，initialPage被忽略，如果为 false 。下次总是从 initialPage 开始。
+        viewportFraction: 0.85, //默认1，每个页面占可视窗的比例
+      ),
       children: <Widget>[
         Container(
           alignment: Alignment.center,
