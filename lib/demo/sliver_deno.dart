fbc41b2a426b908461f8bad8a2983d49e68bd25d
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-16 01:07:10 +0800

    SliverList

--- lib/demo/sliver_deno.dart
+++ lib/demo/sliver_deno.dart
@@ -34,12 +34,40 @@ class SliverListDemo extends StatelessWidget {
             child: Material(
               borderRadius: BorderRadius.circular(12.0),
               elevation: 14.0,
-              shadowColor: Colors.grey.withOpacity(0.6),
-              child: Container(
-                child: Image.network(
-                  posts[index].imageUrl,
-                  fit: BoxFit.cover,
-                ),
+              shadowColor: Colors.grey.withOpacity(0.5),
+              child: Stack(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Image.network(
+                      posts[index].imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    top: 32.0,
+                    left: 32.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          posts[index].title,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          posts[index].title,
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
           );
