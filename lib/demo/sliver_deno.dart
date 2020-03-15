1f02614fc8668719b66e78c7975b875965e0e53f
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-16 01:19:04 +0800

    图片圆角修复

--- lib/demo/sliver_deno.dart
+++ lib/demo/sliver_deno.dart
@@ -5,6 +5,9 @@ class SliverDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('data'),
+      ),
       body: CustomScrollView(
         slivers: <Widget>[
           SliverSafeArea(
@@ -31,43 +34,47 @@ class SliverListDemo extends StatelessWidget {
         (BuildContext context, int index) {
           return Padding(
             padding: EdgeInsets.only(bottom: 32.0),
-            child: Material(
-              borderRadius: BorderRadius.circular(12.0),
-              elevation: 14.0,
-              shadowColor: Colors.grey.withOpacity(0.5),
-              child: Stack(
-                children: <Widget>[
-                  AspectRatio(
-                    aspectRatio: 16 / 9,
-                    child: Image.network(
-                      posts[index].imageUrl,
-                      fit: BoxFit.cover,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Material(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(50.0), // child有图片后这个圆角会失效
+                elevation: 14.0,
+                shadowColor: Colors.grey.withOpacity(0.5),
+                child: Stack(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 16 / 9,
+                      child: Image.network(
+                        posts[index].imageUrl,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  Positioned(
-                    top: 32.0,
-                    left: 32.0,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          posts[index].title,
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            color: Colors.white,
+                    Positioned(
+                      top: 32.0,
+                      left: 32.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            posts[index].title,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                        Text(
-                          posts[index].title,
-                          style: TextStyle(
-                            fontSize: 13.0,
-                            color: Colors.white,
+                          Text(
+                            posts[index].title,
+                            style: TextStyle(
+                              fontSize: 13.0,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
