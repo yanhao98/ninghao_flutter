b8712d3c0c9ce0b39f99180d97b5c171e1bde737
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-18 13:52:41 +0800

    bottomNavigationBar

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -22,9 +22,34 @@ class MaterialComponents extends StatelessWidget {
 class FloatingActionButtonDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Widget _floatingActionButton = FloatingActionButton(
+      onPressed: () {},
+      child: Icon(Icons.add),
+      elevation: 0.0,
+      backgroundColor: Colors.black87,
+      // shape: BeveledRectangleBorder(
+      //   borderRadius: BorderRadius.circular(10.0),
+      // ),
+    );
+
+    final Widget _floatingActionButtonExtended = FloatingActionButton.extended(
+      onPressed: () {},
+      label: Text('Add'),
+      icon: Icon(Icons.add),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('FloatingActionButtonDemo'),
+      ),
+      floatingActionButton: _floatingActionButton,
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 80,
+        ),
+        shape: CircularNotchedRectangle(),
       ),
     );
   }
