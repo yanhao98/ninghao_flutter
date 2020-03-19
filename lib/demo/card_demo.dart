c77243ca94f40b72310325f450dc2e0b9cd7fc22
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-19 19:21:08 +0800

    fix

--- lib/demo/card_demo.dart
+++ lib/demo/card_demo.dart
@@ -10,43 +10,46 @@ class _CardDemoState extends State<CardDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('CardDemo'),
-        elevation: 0.0,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: ListView(
-          children: posts.map((post) {
-            return Card(
-              child: Column(
-                children: <Widget>[
-                  AspectRatio(
-                    aspectRatio: 16/9,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(4.0),
-                        topRight: Radius.circular(4.0),
-                      ),
-                      child: Image.network(
-                        post.imageUrl,
-                        fit: BoxFit.cover,
+        appBar: AppBar(
+          title: Text('CardDemo'),
+          elevation: 0.0,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: ListView(
+            children: posts.map((post) {
+              return Card(
+                child: Column(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 16 / 9,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                        child: Image.network(
+                          post.imageUrl,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(post.imageUrl),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(post.imageUrl),
+                      ),
+                      title: Text(post.title),
+                      subtitle: Text(post.author),
                     ),
-                    title: Text(post.title),
-                    subtitle: Text(post.author),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(post.description, maxLines: 2, overflow: TextOverflow.ellipsis,),
-                  ),
-                  ButtonTheme.bar(
-                    child: ButtonBar(
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        post.description,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    ButtonBar(
                       children: <Widget>[
                         FlatButton(
                           child: Text('Like'.toUpperCase()),
@@ -58,13 +61,11 @@ class _CardDemoState extends State<CardDemo> {
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
-      )
-    );
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ));
   }
 }
