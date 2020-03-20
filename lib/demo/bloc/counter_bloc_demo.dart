a4743e284050214a4ae68d84f8304baa5933f113
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-20 11:50:50 +0800

    创建与使用 BLoC：用 Sink 输入数据

--- lib/demo/bloc/counter_bloc_demo.dart
+++ lib/demo/bloc/counter_bloc_demo.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class CounterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CounterBloc _counterBloc = CounterProvider.of(context).bloc;
+
     return Center(
       child: ActionChip(
         label: Text('0'),
-        onPressed: () {},
+        onPressed: () {
+          // _counterBloc.log();
+          _counterBloc.counter.add(1);
+        },
       ),
     );
   }
@@ -15,9 +22,48 @@ class CounterHome extends StatelessWidget {
 class CounterActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CounterBloc _counterBloc = CounterProvider.of(context).bloc;
+
     return FloatingActionButton(
       child: Icon(Icons.add),
-      onPressed: () {},
+      onPressed: () {
+        // _counterBloc.log();
+        _counterBloc.counter.add(1);
+      },
     );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  CounterProvider({Key key, this.child, this.bloc})
+      : super(key: key, child: child);
+
+  final Widget child;
+  final CounterBloc bloc;
+
+  static CounterProvider of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<CounterProvider>());
+  }
+
+  @override
+  bool updateShouldNotify(CounterProvider oldWidget) {
+    return true;
+  }
+}
+
+class CounterBloc {
+  final _counterActionController = StreamController<int>();
+  StreamSink<int> get counter => _counterActionController.sink;
+
+  CounterBloc() {
+    _counterActionController.stream.listen(onData);
+  }
+
+  void log() {
+    print('BLoC');
+  }
+
+  void onData(int data) {
+    print('$data');
   }
 }
