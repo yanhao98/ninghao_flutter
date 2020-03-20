cef4191a2e3302991e600987e51fb7d869a2674c
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-20 11:15:04 +0800

    RxDart 的数据转换方法（map）

--- lib/demo/rxdart/rxdart_demo.dart
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -22,9 +22,16 @@ class RxDartDemoHome extends StatefulWidget {
 }
 
 class _RxDartDemoHomeState extends State<RxDartDemoHome> {
+  PublishSubject<String> _textFieldSubject;
+
   @override
   void initState() {
     super.initState();
+
+    _textFieldSubject = PublishSubject<String>();
+    _textFieldSubject.map((item) => '处理后的数据：$item').listen((data) {
+      print(data);
+    });
 
     // Stream<String> _observable =
     // Stream.fromIterable(['hello', '您好']);
@@ -35,22 +42,42 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
     // _observable.listen(print);
     // PublishSubject<String> _subject = PublishSubject<String>();
     // BehaviorSubject<String> _subject = BehaviorSubject<String>();
-    ReplaySubject<String> _subject = ReplaySubject<String>(maxSize: 1);
-    _subject.add('hello');
-    _subject.add('hello2');
-    _subject.listen((value) {
-      print('listen 1: $value');
-    });
-    _subject.listen((value) {
-      print('listen 2: ${value.toUpperCase()}');
-    });
-    _subject.add('hola');
+    // ReplaySubject<String> _subject = ReplaySubject<String>(maxSize: 1);
+    // _subject.add('hello');
+    // _subject.add('hello2');
+    // _subject.listen((value) {
+    //   print('listen 1: $value');
+    // });
+    // _subject.listen((value) {
+    //   print('listen 2: ${value.toUpperCase()}');
+    // });
+    // _subject.add('hola');
 
-    _subject.close();
+    // _subject.close();
+  }
+
+  @override
+  void dispose() {
+    _textFieldSubject.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Theme(
+      data: Theme.of(context).copyWith(primaryColor: Colors.black),
+      child: TextField(
+        onChanged: (value) {
+          _textFieldSubject.add('input: $value');
+        },
+        onSubmitted: (value) {
+          _textFieldSubject.add('submit: $value');
+        },
+        decoration: InputDecoration(
+          labelText: 'Title',
+          filled: true,
+        ),
+      ),
+    );
   }
 }
