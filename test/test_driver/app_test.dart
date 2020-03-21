8d4059fd0a44f0b6e0ccb951c317a0f1f813d9c2
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-21 10:46:01 +0800

    添加继承测试

---
+++ test/test_driver/app_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('App', () {
+    FlutterDriver driver;
+
+// 运行之前做的事情
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+  });
+}
