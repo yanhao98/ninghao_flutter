f5db05127ae3149e6b28e110d784e8bfa8cbd1cb
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-21 18:45:24 +0800

    flutter sdk 降级

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
+// import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart' hide find;
 
 void main() {
   group('App', () {
